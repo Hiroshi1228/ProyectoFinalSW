8d929b9f41316bc472dde7532bd9ac5e20c611f0
Yielcy <jonthan.galio108@gmail.com>
2021-12-10 01:51:28 -0600

    Agregar pregunta abierta

--- proyecto/src/main/resources/insertar.js
+++ proyecto/src/main/resources/insertar.js
@@ -18,6 +18,33 @@ function insertarRadio() {
     lista.appendChild(contene);
 }
 
-function insertarPreguntaC() {
-    
+function insertarPreguntaA() {
+    var preguntaA = document.getElementById('pregunta').value;
+    var tipoP = document.getElementById('tipoPreguntas').value;
+    var areaT = document.getElementById('areaText').value;
+    var videoG = document.getElementById('video').value;
+
+    const listaA = document.getElementById('contedorPreguntas');
+    const nuevoDiv = document.createElement('div');
+    nuevoDiv.className = 'w-3/6 m-auto py-4';
+    nuevoDiv.innerHTML = '<div class="flex flex-wrap w-full m-auto">'+
+                            '<div class="w-2/3 px-4 m-auto">'+
+                                '<input type="text" name="pregunta" id="pregunta" placeholder="  Pregunta:" class="w-4/5 border" value="'+preguntaA+'" disabled>'+
+                            '</div>'+
+                            '<div class="w-1/3 pl-2 pr-8">'+
+                                '<select name="tipoPreguntas" id="tipoPreguntas" class="w-40">'+
+                                    '<option value="'+tipoP+'">Pregunta abierta</option>'+
+                                '</select>'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="w-auto mt-4 p-2 text-center">'+
+                            '<textarea name="areaText" id="areaText" cols="55" rows="4" class="border border-solid" placeholder=" Respuesta:" value="" disabled>'+areaT+'</textarea>'+
+                            '<div class="w-full py-2">'+
+                                '<input type="file" name="video" id="video" accept=".mp4"  value="'+videoG+'" class="" disabled>'+videoG+
+                            '</div>'+
+                            '<div class="w-full mt-4 ml-56 mb-4">'+
+                                '<button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" type="submit" disabled> Agregar </button>'+
+                            '</div>'+
+                        '</div>';
+    listaA.appendChild(nuevoDiv);
 }
